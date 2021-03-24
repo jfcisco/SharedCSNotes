60437a90c27435f4b7532dddb292c576bbd029f6
Jasper Francisco <jasper.francisco@gmail.com>
2021-03-24 20:27:28 +0800

    Change comment location

--- CMSC204/EX02LinkedLists/Program.cs
+++ CMSC204/EX02LinkedLists/Program.cs
@@ -21,7 +21,21 @@ namespace EX02LinkedLists
             bobStudent.Data.MobileNumber = "8132";
 
             testList.InsertEnd(bobStudent);
+
+            // Test the Insert and Search methods.
             Console.WriteLine(testList.Search("Anna").Data.MobileNumber);
+
+            // Test the Delete method implementation for all possible cases:
+
+            /*
+                Cases to consider:
+                ○ The list is empty;
+                ○ The node to remove is the only node in the linked list;
+                ○ We are removing the head node;
+                ○ We are removing the tail node;
+                ○ The node to remove is somewhere in between the head and tail;
+                ○ The item to remove doesn’t exist in the linked list
+            */
         }
     }
 
@@ -101,16 +115,6 @@ namespace EX02LinkedLists
         /// Deletes the given Node in the linked list
         public void Delete(Node nodeToDelete)
         {
-            /*
-                Cases to consider:
-                ○ The list is empty;
-                ○ The node to remove is the only node in the linked list;
-                ○ We are removing the head node;
-                ○ We are removing the tail node;
-                ○ The node to remove is somewhere in between the head and tail;
-                ○ The item to remove doesn’t exist in the linked list
-            */
-
             // List is empty -> Do nothing.
             if (Header != null)
             {
