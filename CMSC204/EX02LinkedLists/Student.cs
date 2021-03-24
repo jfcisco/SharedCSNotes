53c4f8dc97b9db2089fd08dd32ef757e72e5d8cb
Jasper Francisco <jasper.francisco@gmail.com>
2021-03-24 20:32:57 +0800

    Put type in own file

---
+++ CMSC204/EX02LinkedLists/Student.cs
@@ -0,0 +1,10 @@
+namespace EX02LinkedLists
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Nickname { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
