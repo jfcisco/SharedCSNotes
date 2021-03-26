4f6a80b2b879244d289e05ac522d14a609575668
Jasper Francisco <jasper.francisco@gmail.com>
2021-03-26 20:00:43 +0800

    Add testing code for Binary Tree

---
+++ CMSC204/Ex04BinaryTree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex04BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var originalBTree = new BinaryTree(10);
+
+            originalBTree.Add(2);
+            originalBTree.AddRc(20);
+            originalBTree.AddRc(15);
+            originalBTree.AddRc(15);
+            originalBTree.AddRc(-2);
+            originalBTree.AddRc(2);
+
+            // string treeData = "";
+            // originalBTree.Print(null, ref treeData);
+            // Console.WriteLine(treeData);
+
+            System.Console.WriteLine(originalBTree.PrintReverse());
+        }
+
+        static void ModifyReference(ref int i)
+        {
+            i = i + 1;
+        }
+    }
+}
