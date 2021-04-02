5988cf35f19bd5a9c76caccb946faa9f0fb8c8ec
Jasper Francisco <jasper.francisco@gmail.com>
2021-04-02 16:16:16 +0800

    Apply naming convention to private vars

--- CMSC204/Ex03StacksQueues/ArrayQueue.cs
+++ CMSC204/Ex03StacksQueues/ArrayQueue.cs
@@ -6,35 +6,37 @@ namespace Ex03StacksQueues
     public class ArrayQueue
     {
         public string[] Data { get; set; }
-        int Head;
-        int Tail;
+        private int _head;
+        private int _tail;
+        private int _length;
         
-        public ArrayQueue(int size)
+        public ArrayQueue(int length)
         {
-            Data = new string[size];
+            Data = new string[length];
             
+            _length = length;
             // Initialize Head and Tail to last index of Data.
-            Head = size - 1;
-            Tail = size - 1;
+            _head = length - 1;
+            _tail = length - 1;
         }
 
         /// <summary>Inserts an item at the back of the queue.</summary>
         public void Insert(string dataToInsert)
         {
-            if (Head == Tail && Data[Tail] != null)
+            if (_head == _tail && Data[_tail] != null)
             {
                 throw new Exception("Queue overflow.");
             }
 
-            Data[Tail] = dataToInsert;
+            Data[_tail] = dataToInsert;
             
-            if (Tail == Data.Length - 1)
+            if (_tail == _length - 1)
             {
-                Tail = 0;
+                _tail = 0;
             }
             else
             {
-                Tail += 1;
+                _tail += 1;
             }
 
             /* 
@@ -62,23 +64,23 @@ namespace Ex03StacksQueues
         /// <summary>Returns and removes the item at the front of the queue.</summary>
         public string Remove()
         {
-            if (Head == Tail && Data[Head] == null)
+            if (_head == _tail && Data[_head] == null)
             {
                 throw new Exception("Queue underflow.");
             }
 
             // Temporarily store data to be returned later on, then set head to null.
-            string dataAtHead = Data[Head];
-            Data[Head] = null;
+            string dataAtHead = Data[_head];
+            Data[_head] = null;
 
             // Move head to next item
-            if (Head == Data.Length - 1)
+            if (_head == _length - 1)
             {
-                Head = 0;
+                _head = 0;
             }
             else
             {
-                Head += 1;
+                _head += 1;
             }
 
             return dataAtHead;
