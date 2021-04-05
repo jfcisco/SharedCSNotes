a34468d4971d302c886ce3af898a2dee3c64c452
Jasper Francisco <jasper.francisco@gmail.com>
2021-04-05 20:54:40 +0800

    Implement GetMax and GetMin methods

--- CMSC204/Ex05BSTAVL/BST.cs
+++ CMSC204/Ex05BSTAVL/BST.cs
@@ -11,7 +11,9 @@ namespace Ex05BSTAVL
 
         public BST(int initialValue)
         {
-            var initialNode = new Node(initialValue)
+            // Instatiate new node for the head.
+            // Parent = null since it is the head node.
+            var initialNode = new Node(initialValue, null)
             {
                 Left = null,
                 Right = null
@@ -22,7 +24,11 @@ namespace Ex05BSTAVL
 
         public void Add(int value)
         {
-            // Recursive add method interface
+            /*
+                Cases to consider:
+                1. Insert into an empty tree.
+                2. Insert into a non-empty tree.
+            */
             Add(ref _head, value);
         }
 
@@ -31,7 +37,8 @@ namespace Ex05BSTAVL
             // Private recursive search for where to add the new node
             if (node == null)
             {
-                node = new Node(value);
+                // Base case for recursion
+                node = new Node(value, null);
                 return;
             }
             else if (value < node.Value)
@@ -40,25 +47,58 @@ namespace Ex05BSTAVL
                 // So, node.Left cannot be passed directly in a ref parameter
                 Node tempNode = node.Left;
                 Add(ref tempNode, value);
+                    
+                tempNode.Parent = node;     // Update new node's parent pointer
                 node.Left = tempNode;
             }
             else if (value >= node.Value)
             {
                 Node tempNode = node.Right;
                 Add(ref tempNode, value);
+
+                tempNode.Parent = node;     // Update new node's parent pointer
                 node.Right = tempNode;
             }
+        }
+
+        public int GetMinimum()
+        {
+            // Get the left-most node
+            Node currentNode = _head;
+            
+            while (currentNode.Left != null)
+            {
+                currentNode = currentNode.Left;
+            }
+
+            return currentNode.Value;
+        }
+
+        public int GetMaximum()
+        {
+            // Get the right-most node
+            Node currentNode = _head;
+
+            while (currentNode.Right != null)
+            {
+                currentNode = currentNode.Right;
+            }
+
+            return currentNode.Value;
+            
         }
 
         private class Node
         {
             public int Value;
+            public Node Parent;
             public Node Left;
             public Node Right;
 
-            public Node(int initialValue)
+            public Node(int initialValue, Node parent)
             {
                 Value = initialValue;
+                Parent = parent;
                 Left = null;
                 Right = null;
             }
