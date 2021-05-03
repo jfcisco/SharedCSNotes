bb697dccbf100fb82d7a9a94b68fb29f158e0071
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-03 18:58:57 +0800

    Add simple test for graph

--- CMSC204/Ex08Graphs/Program.cs
+++ CMSC204/Ex08Graphs/Program.cs
@@ -6,7 +6,18 @@ namespace Ex08Graphs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+
+            MatrixGraph letters = new MatrixGraph();
+            
+            letters.AddVertex('A');
+            letters.AddVertex('B');
+            letters.AddVertex('C');
+
+            letters.AddEdge('A', 'B');
+            letters.AddEdge('A', 'C');
+
+            Console.WriteLine(letters);
         }
     }
 }
